6c4db08b2ec838244d89641be7f4bdc4d27a6cef
Juho Torkkeli <juho.torkkeli@icloud.com>
2022-02-06 13:03:18 +0200

    change name and icon

---
+++ driving_time_log/lib/resources/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+var darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: Colors.lightBlue[800],
+  backgroundColor: const Color(0xFF333333),
+
+  // Define the default font family.
+  fontFamily: 'Roboto',
+
+  // Define the default `TextTheme`. Use this to specify the default
+  // text styling for headlines, titles, bodies of text, and more.
+  textTheme: const TextTheme(
+    headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+    headline2: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+    bodyText1: TextStyle(fontSize: 14.0),
+  ),
+);
