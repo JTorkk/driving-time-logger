ba4911adc1d106400eca048e486498c8fe2a9126
Juho Torkkeli <juho.torkkeli@icloud.com>
2022-02-06 12:15:15 +0200

    intial commit

---
+++ driving_time_log/android/app/src/main/kotlin/com/example/driving_time_log/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.driving_time_log
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ driving_time_log/lib/resources/assets.dart
@@ -0,0 +1,2 @@
+const car = 'dassets/car.svg';
+const notepad = 'dassets/notepad.svg';
